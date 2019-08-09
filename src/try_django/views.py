6905a59c4466dfe618f51239744f802bb91d8726
strongSoda <strongsoda2@gmail.com>
2019-08-09 23:17:39 +0530

    Day 0

---
+++ src/try_django/views.py
@@ -0,0 +1,13 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def home_page(request):
+    return render(request, "hello_world.html")
+
+
+def about_page(request):
+    return HttpResponse("<h1>About Us<h1>")
+
+
+def contact_page(request):
+    return HttpResponse("<h1>Contact Us<h1>")
